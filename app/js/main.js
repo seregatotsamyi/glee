4f1a29552fdf84997ce4969b13089f792601dae4
seregatotsamyi <iserg.mos.ls@gmail.com>
2020-12-01 16:52:10 +0500

    start filter shop

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,19 @@ $(function(){
     fade: true,
   });
  
-    
+    $('.filter-price__input').ionRangeSlider({
+      type: "double",
+      prefix: "$",
+      step: 0.01,
+      onStart: function (data){
+        $('.filter-price__from').text(data.from);
+        $('.filter-price__to').text(data.to);
+      },
+      onChange: function (data) {
+        $('.filter-price__from').text(data.from);
+        $('.filter-price__to').text(data.to);
+    },
+    });
 
 
   var mixer = mixitup('.productsweek__inner' , {
